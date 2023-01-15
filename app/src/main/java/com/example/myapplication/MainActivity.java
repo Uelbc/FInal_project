f22f3f7276efdca866642f2747890d7dcf789033
aleks <aleksejgudis623@gmail.com>
2023-01-15 21:24:48 +0300

    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,76 +1,62 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    private SeekBar seekBar1;
-    private SeekBar seekBar2;
-    private SeekBar seekBar3;
-    private TextView textView1;
-    private TextView textView2;
-    private TextView textView3;
-
+    public EditText weight_edittext;
+    public EditText height_edittext;
+    public EditText age_edittext;
+    public AppCompatButton register;
+    public RadioButton men;
+    public RadioButton women;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        seekBar1=findViewById(R.id.seekBar);
-        seekBar2=findViewById(R.id.seekBar4);
-        seekBar3=findViewById(R.id.seekBar6);
-        textView1=findViewById(R.id.textView11);
-        textView2=findViewById(R.id.textView12);
-        textView3=findViewById(R.id.textView13);
-        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        AppCompatButton register = findViewById(R.id.register);
+        weight_edittext = findViewById(R.id.editTextTextPersonName);
+        height_edittext = findViewById(R.id.editTextTextPersonName2);
+        age_edittext = findViewById(R.id.editTextTextPersonName3);
+        men = findViewById(R.id.radioButton);
+        women = findViewById(R.id.radioButton2);
+        men.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textView1.setText(String.valueOf(seekBar1.getProgress()));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View view) {
+                women.setChecked(false);
+                men.setChecked(true);
             }
         });
-        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        women.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textView2.setText(String.valueOf(seekBar2.getProgress()));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View view) {
+                men.setChecked(false);
+                women.setChecked(true);
             }
         });
-        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textView3.setText(String.valueOf(seekBar3.getProgress()));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View view) {
+                int w, h, a, g;
+                w = Integer.parseInt(weight_edittext.getText().toString());;
+                h = Integer.parseInt(height_edittext.getText().toString());
+                a = Integer.parseInt(age_edittext.getText().toString());
+                if (men.isChecked()){
+                    g=1;
+                } else{
+                    g=0;
+                }
+                int [] weight_height_age_gender = new int[] {w, h, a, g};
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("1", weight_height_age_gender);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    int [] a;
+    public TextView kal_per_day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            a = bundle.getIntArray("1");
+        }
+        int weight = a[0];
+        int height = a[1];
+        int age = a[2];
+        int gender = a[3]; // 1-мужчина 0-женщина
+        kal_per_day = findViewById(R.id.kal_per_day);
+        if (gender==1){//если мужчина
+            kal_per_day.setText(Integer.toString((int)(10*weight+6.25*height-5*age+5)));
+        }else{
+            kal_per_day.setText(Integer.toString((int)(10*weight+6.25*height-5*age-161)));
+        }
     }
 }
