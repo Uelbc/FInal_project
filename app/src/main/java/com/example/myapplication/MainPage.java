b6ccc1220cb25c03c02860b23a5672d432139f88
aleks <aleksejgudis623@gmail.com>
2023-01-18 00:29:16 +0300

    Added food page

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -2,12 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainPage extends AppCompatActivity {
     double [] weight_height_age_gender_A;
     public TextView kal_per_day;
+    public Button food;
+    public String message_to_food;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +32,15 @@ public class MainPage extends AppCompatActivity {
         }else{
             kal_per_day.setText(Integer.toString((int)((10*weight+6.25*height-5*age-161)*A)));
         }
+        food = findViewById(R.id.food);
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message_to_food="";
+                Intent intent = new Intent(MainPage.this, Food.class);
+                intent.putExtra("From main menu", message_to_food);
+                startActivity(intent);
+            }
+        });
     }
 }
